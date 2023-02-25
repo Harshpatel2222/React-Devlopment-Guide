45a6a79db9f3d0b901218cc8df3e46312099e43b
Harsh A Patel <harsh.a.patel@biznovare.com>
2023-02-25 20:18:12 +0530

    added chart

---
+++ react-complete-guide/src/Components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ChartBar from "./ChartBar"
+
+import './Chart.css'
+
+const Chart = (props) => {
+    return(
+        <div className="chart">
+            {props.dataPoints.map((dataPoints) => {
+                <ChartBar 
+                    key={dataPoints.lable}
+                    value={dataPoints.value}
+                    maxValue={null}
+                    lable={dataPoints.lable}
+                />
+            })}
+
+        </div>
+    )
+}
+
+export default Chart;
